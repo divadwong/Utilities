7cd4319d60a61a0974d2178c1a6647a3f7ff60ad
divadwong <40673755+divadwong@users.noreply.github.com>
2020-10-16 13:23:14 -0400

    Update Get-ADUserInfo.ps1

--- Get-ADUserInfo.ps1
+++ Get-ADUserInfo.ps1
@@ -4,7 +4,7 @@ Param(
 	[AllowEmptyCollection()]
 	[array]$users,
 	# Change InputType that will be processed
-	[Parameter(Mandatory = $false)][ValidateSet("SAmAccountName","DistinguishedName","UserPrincipalName")][string]$InputType="SAmAccountName"
+	[Parameter(Mandatory = $false)][ValidateSet("SAmAccountName","DistinguishedName","UserPrincipalName","Name")][string]$InputType="SAmAccountName"
 )
 
 function ProcessList{
@@ -12,23 +12,25 @@ Param($InputType)
 	$userList = @{}
 	$userobj = @()
 	
-	Write-Host "InputType set to $InputType"
 	foreach ($user in $users)
 	{
 		# Trim spaces in front and end
 		$user=$user.Trim()
 		# check to see if Input is valid. Add to HT if valid,
 		$Filter = "(" + $Inputtype + "=" + $user + ")"
-		$userobj = Get-ADUser -LDAPFilter $Filter -Properties givenName, sn, Enabled, AccountExpires, Mail, department, streetAddress, telephoneNumber, title, lastLogonTimestamp, lastLogon, userAccountControl, employeeNumber, whenCreated, pwdLastSet, passwordNeverExpires |
-		Select-Object DistinguishedName, Enabled, SamAccountName, Name, givenName, sn, Mail, employeeNumber, whenCreated, department, title, streetAddress, telephoneNumber, @{N='AccountExpires'; E={[DateTime]::FromFileTime($_.AccountExpires).ToString('g')}}, @{N='lastLogonTimestamp'; E={[DateTime]::FromFileTime($_.lastLogonTimestamp).ToString('s')}}, @{N='lastLogon'; E={[DateTime]::FromFileTime($_.lastLogon).ToString('g')}}, @{N='pwdLastSet'; E={[DateTime]::FromFileTime($_.pwdLastSet).ToString('s')}}, passwordNeverExpires | Sort-Object SamAccountName
-		if ($userobj -ne $null) {$userList.add($user,$userobj) | Out-Null}
+		$userobj = Get-ADUser -LDAPFilter $Filter -Properties givenName, sn, Enabled, AccountExpires, Mail, department, streetAddress, telephoneNumber, title, lastLogonTimestamp, lastLogon, userAccountControl, employeeNumber, whenCreated, pwdLastSet, passwordNeverExpires, ProfilePath |
+		Select-Object DistinguishedName, Enabled, SamAccountName, Name, givenName, sn, Mail, employeeNumber, whenCreated, department, title, streetAddress, telephoneNumber, @{N='AccountExpires'; E={[DateTime]::FromFileTime($_.AccountExpires).ToString('g')}}, @{N='lastLogonTimestamp'; E={[DateTime]::FromFileTime($_.lastLogonTimestamp).ToString('s')}}, @{N='lastLogon'; E={[DateTime]::FromFileTime($_.lastLogon).ToString('g')}}, @{N='pwdLastSet'; E={[DateTime]::FromFileTime($_.pwdLastSet).ToString('s')}}, passwordNeverExpires, ProfilePath
+		if ($userobj) {$userList.add($user,$userobj) | Out-Null}
 			else
-		{write-host $user "is invalid"}
+		{
+			Write-host "$user not found and logged to $location\results\GetInfo_BadID.csv"
+			$Date = (get-date -format filedatetime)[0..12] -join ''; $Date +"`t"+ $Inputtype +"`t"+ $user | Out-File $location\results\GetInfo_BadID.csv -Append
+		}
 	}
 	return $userlist.values
 }
 
-# Called from GetUserInfo.cmd
+# Called from GetUserInfo.cmd and FindUser.cmd
 $location = Split-Path $PSCommandPath -Parent
 # If No Input process the Userlist-Info.txt
 if (-Not($users))
@@ -39,29 +41,37 @@ if (-Not($users))
 	$users = Get-Content $location\Userlist-Info.txt
 }
 
+if($InputType -ne "SAmAccountName"){write-host "InputType set to $InputType"}
 write-host "Generating user info report ....."
 # Process users 
-$ProcessededUsers = ProcessList $InputType
-if ($ProcessededUsers)
+$ProcessedUsers = ProcessList $InputType
+if ($ProcessedUsers)
 {
-	$ProcessededUsers = $ProcessededUsers | Sort SAmAccountName
-	write-host @($ProcessededUsers).count users
+    if($ProcessedUsers.Syncroot.count -gt 0)  # If used multiple wildcards, the array will be split into syncroot. Need to combined them.
+    {
+      $CombineThem = @()
+      For ($i=0; $i -le $ProcessedUsers.Syncroot.Count-1; $i++){$CombineThem += $ProcessedUsers.Syncroot[$i]}
+      $ProcessedUsers = $CombineThem
+    }
+	$ProcessedUsers = $ProcessedUsers | Sort SAmAccountName
+	$Count = $ProcessedUsers.SamAccountName.Count
+	write-host $Count users
 	$Output = Read-Host -Prompt 'Output to Screen or File? (S, F) Screen is default'
 	if ($Output -eq "F")
 	{
-		if (($ProcessededUsers.count) -gt 1)
+		if (($Count) -gt 1)
 		{	
-			$ProcessededUsers | Export-CSV -Path $location\UserInfo.csv -NoTypeInformation
+			$ProcessedUsers | Export-CSV -Path $location\results\UserInfo.csv -NoTypeInformation
 			write-host $location\results\User-Info.csv created
 		}
 		else
 		{		
-			$ProcessededUsers | Export-CSV -Path $location\$Users.csv -NoTypeInformation
+			$ProcessedUsers | Export-CSV -Path $location\results\$Users.csv -NoTypeInformation
 			write-host $location\results\$Users.csv created
 		}
 	}	
 	else
-	{$ProcessededUsers}
+	{$ProcessedUsers}
 }
 else
 {write-host "No Users processed"}
