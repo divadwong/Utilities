d8b041691dbefd61286606f672313c7ab7fb8933
divadwong <40673755+divadwong@users.noreply.github.com>
2021-03-14 13:11:06 -0400

    Update Add-ItemToReg.ps1

--- Add-ItemToReg.ps1
+++ Add-ItemToReg.ps1
@@ -3,10 +3,12 @@ function Add-ItemToReg
 param(
 [Parameter(Mandatory = $true)][String]$Key,
 [Parameter(Mandatory = $true)][String]$Name,
-[Parameter(Mandatory = $true)][String]$Item
+[Parameter(Mandatory = $true)][String]$Item,
+[Parameter(Mandatory = $false)][Switch]$AppendSemi
 )
 
 	$CheckItem = @(); $AddItem = ''
+	if($AppendSemi){$Semi = ";"}
 	$GetNameInfo = Get-ItemProperty $Key -Name $Name -EA SilentlyContinue
 	if($GetNameInfo)
 	{$CheckItem = (($GetNameInfo.$Name).Split(';') -ne '').Trim() | Sort -Unique}
@@ -14,8 +16,8 @@ param(
 	{New-Item -Path $Key -Force -EA Continue | Out-Null}
 	if($CheckItem -notcontains $Item)
 	{
-		if($CheckItem){$AddItem = ($CheckItem -join ';') + ";$Item"}
-		else{$AddItem = $Item}
+		if($CheckItem){$AddItem = ($CheckItem -join ';') + ";$Item"+$Semi}
+		else{$AddItem = $Item + $Semi}
 	
 		try{
 			New-ItemProperty -Path $Key -Name $Name -PropertyType String -Value $AddItem -Force -EA Stop | Out-Null
@@ -30,5 +32,6 @@ param(
 	{Write-Host "$Item already in $Key\$Name"}
 }
 
-#  Add an Item to a registry string. ie. below ex. will append to HKCU:\PathTest\Path = c:\path1;c:\path2;c:\path3
+# Add an Item to a registry string. ie. below ex. will append to HKCU:\PathTest\Path = c:\path1;c:\path2;c:\path3
+# Optional switch -AppendSemi will at a semicolon at end of the string. ie. HKCU:\PathTest\Path = c:\path1;c:\path2;c:\path3;
 Add-ItemToReg -Key HKCU:\PathTest -Name Path -Item c:\path4
