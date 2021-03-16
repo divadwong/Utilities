072fa752f5910d225617ca5dbb5de3c971e38e8f
divadwong <40673755+divadwong@users.noreply.github.com>
2021-03-16 10:31:15 -0400

    Update Add-ItemToListInReg.ps1

--- Add-ItemToListInReg.ps1
+++ Add-ItemToListInReg.ps1
@@ -15,24 +15,44 @@ param(
 	$CheckItem = @(); $AddItem = ''
 	if($AppendSemi){$Semi = ";"}
 	$GetNameInfo = Get-ItemProperty $Key -Name $Name -EA 0
+	
 	if($GetNameInfo)
-	{$CheckItem = (($GetNameInfo.$Name).Split(';') -ne '').Trim() | Sort -Unique}    # Remove duplicates, spaces and double semicolons from list
+	{
+		$Prev = "was " + $GetNameInfo.$Name
+		$CheckItem = (($GetNameInfo.$Name).Split(';') -ne '').Trim() | Sort -Unique # Remove duplicates, spaces and double semicolons from list
+	}
 	else
-	{New-Item -Path $Key -Force -EA 0 | Out-Null}                                    # Create Registry Key if it does not exist
+	{   # Create Registry Key if it does not exist
+		if(!(Test-Path $Key)){New-Item -Path $Key -Force -EA 0 | Out-Null ; $Prev = "did not exist and was created"}
+		else{$Prev = "did not exist"}
+	}                             
 	if($CheckItem -notcontains $Item)
-	{  # Append Item to end of string. Also add semicolon to end if -AppendSemi parameter used.
-		if($CheckItem){$AddItem = ($CheckItem -join ';') + ";$Item"+$Semi}       # Add to existing list 
-		else{$AddItem = $Item + $Semi}                                           # Add to empty list
+	{   # Append Item to end of string. Also add semicolon to end if -AppendSemi parameter used.
+		if($CheckItem){$AddItem = ($CheckItem -join ';') + ";$Item"+$Semi}        # Add to existing list 
+		else{$AddItem = $Item+$Semi}                                              # Add to empty list
 	
 		try{
 			New-ItemProperty -Path $Key -Name $Name -PropertyType String -Value $AddItem -Force -EA Stop | Out-Null
-			Write-Host "$Key\$Name set to $AddItem"
+			WriteAppNameLog "$Key\$Name previously $Prev"
+			WriteAppNameLog "$Key\$Name set to $AddItem"
+			$script:eMailMessage += "`r$Key\$Name set to $AddItem, "
+			$PromptReboot = $True
 		}
 	
 		catch{
-			Write-Host "Failed to append $Item to $Key\$Name"
+			WriteAppNameLog "Failed to append $Item to $Key\$Name"
+			$script:eMailMessage += "`rFailed to append $Item to $Key\$Name, "
+			$PostInstallStatus = $False
 		}
 	}
 	else
-	{Write-Host "$Item already in $Key\$Name"}
+	{
+		WriteAppNameLog "$Item already in $Key\$Name"
+		$script:eMailMessage += "`r$Item already in $Key\$Name, "
+		$AddItem = ($CheckItem -join ';') + $Semi
+		New-ItemProperty -Path $Key -Name $Name -PropertyType String -Value $AddItem -Force -EA Stop | Out-Null
+		WriteAppNameLog "$Key\$Name previously $Prev"
+		WriteAppNameLog "Cleaned up list $Key\$Name to $AddItem"
+	}
 }
+
