92815e96fb73e4d81ccd459fb45a10cb7cbc126d
divadwong <40673755+divadwong@users.noreply.github.com>
2021-03-21 11:44:10 -0400

    Update Add-ItemToListInReg.ps1

--- Add-ItemToListInReg.ps1
+++ Add-ItemToListInReg.ps1
@@ -1,7 +1,7 @@
 # Add an Item to a registry string.
-# ie. Add-ItemToListInReg -Key HKCU:\PathTest -Name Path -Item c:\path3                HKCU:\PathTest\Path = c:\path1;c:\path2;c:\path3
-# Optional switch -AppendSemi will add a semicolon at end of the string. 
-# ie. Add-ItemToListInReg -Key HKCU:\PathTest -Name Path -Item c:\path3 -AppendSemi    HKCU:\PathTest\Path = c:\path1;c:\path2;c:\path3;
+# ie. Add-ItemToListInReg -Key HKCU:\PathTest -Name Path -Item c:\path3               HKCU:\PathTest\Path = c:\path1;c:\path2;c:\path3
+# Optional switch -AppendEnd will add a delimter to the end.
+# ie. Add-ItemToListInReg -Key HKCU:\PathTest -Name Path -Item c:\path3 -AppendEnd    HKCU:\PathTest\Path = c:\path1;c:\path2;c:\path3;
 
 function Add-ItemToListInReg
 {
@@ -9,11 +9,11 @@ param(
 [Parameter(Mandatory = $true)][String]$Key,
 [Parameter(Mandatory = $true)][String]$Name,
 [Parameter(Mandatory = $true)][String]$Item,
-[Parameter(Mandatory = $false)][Switch]$AppendSemi
+[Parameter(Mandatory = $false)][Switch]$AppendEnd
 )
 
+	if($AppendEnd){$Delim = ";"}
 	$CheckItem = @(); $AddItem = ''
-	if($AppendSemi){$Semi = ";"}
 	$GetNameInfo = Get-ItemProperty $Key -Name $Name -EA 0
 	
 	if($GetNameInfo)
@@ -27,9 +27,9 @@ param(
 		else{$Prev = "did not exist"}
 	}                             
 	if($CheckItem -notcontains $Item)
-	{   # Append Item to end of string. Also add semicolon to end if -AppendSemi parameter used.
-		if($CheckItem){$AddItem = ($CheckItem -join ';') + ";$Item"+$Semi}        # Add to existing list 
-		else{$AddItem = $Item+$Semi}                                              # Add to empty list
+	{   # Append Item to end of string. Also add semicolon to end if -AppendEnd parameter used.
+		if($CheckItem){$AddItem = ($CheckItem -join $Delim) + $Delim + "$Item" + $Delim}       # Add to existing list 
+		else{$AddItem = $Item + $Delim}                                                          # Add to empty list
 	
 		try{
 			New-ItemProperty -Path $Key -Name $Name -PropertyType String -Value $AddItem -Force -EA Stop | Out-Null
@@ -44,7 +44,7 @@ param(
 	else
 	{
 		Write-Host "$Item already in $Key\$Name"
-		$AddItem = ($CheckItem -join ';') + $Semi
+		$AddItem = ($CheckItem -join ';') + $Delim
 		New-ItemProperty -Path $Key -Name $Name -PropertyType String -Value $AddItem -Force -EA Stop | Out-Null
 		Write-Host "$Key\$Name previously $Prev"
 		Write-Host "Cleaned up list $Key\$Name to $AddItem"
